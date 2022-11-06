a2e5e133f35dbc05833ec5bc24f56829146b0acb
Juan <juacdiazda@gmail.com>
2022-11-06 14:52:09 -0300

    Responsive

--- src/Layouts/NavLayout.jsx
+++ src/Layouts/NavLayout.jsx
@@ -7,27 +7,29 @@ const NavLayout = () => {
   return (
     <>
         <div className="flex flex-col justify-between min-h-screen">
-            <nav className="bg-indigo-600 flex flex-nowrap flex-col md:flex-row shadow-md justify-between">
+            <nav className="bg-indigo-600 flex  flex-col md:flex-row shadow-md justify-between">
                 <div className="font-bold flex  justify-center text-2xl px-2 py-5 text-white whitespace-nowrap">
                     <h1>Trabajo Práctico Nº5 <span className=" text-slate-100">Cobranzas</span></h1>
                 </div>
-                <div className=" flex flex-row">
-                    <div className="text-white font-bold   hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
-                        <Link className="p-5 px-10" to="Punto1/">1</Link>
+                <div className=" flex flex-row flex-wrap md:flex-nowrap justify-evenly">
+                    <div className=" flex flex-row flex-grow justify-evenly">
+                        <div className="text-white font-bold   hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                            <Link className="p-5 px-10" to="Punto1/">1</Link>
+                        </div>
+                        <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                            <Link className="p-5 px-10" to="Punto2/">2</Link>
+                        </div>
+                        <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                            <Link className="p-5 px-10" to="/Punto3/">3</Link>
+                        </div>
+                        <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                            <Link className="p-5 px-10"  to="Punto4/">4</Link>
+                        </div>
+                        <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                            <Link className="p-5 px-10"  to="Punto5/">5</Link>
+                        </div>
                     </div>
-                    <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
-                        <Link className="p-5 px-10" to="Punto2/">2</Link>
-                    </div>
-                    <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
-                        <Link className="p-5 px-10" to="/Punto3/">3</Link>
-                    </div>
-                    <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
-                        <Link className="p-5 px-10"  to="Punto4/">4</Link>
-                    </div>
-                    <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
-                        <Link className="p-5 px-10"  to="Punto5/">5</Link>
-                    </div>
-                    <div className="text-white font-bold  hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
+                    <div className="text-white font-bold basis-96 hover:bg-indigo-400 transition-colors text-center flex flex-col justify-center">
                         <Link className="p-5 px-10"  to="/">Integrantes</Link>
                     </div>
                 </div>
